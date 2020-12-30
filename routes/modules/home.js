34822a71d0d81430efd085e8dc99c28fd21f0b41
Johnny Chang <zjps3407@gmail.com>
2020-12-30 18:38:45 +0800

    refactor with Promise.all()

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,12 +6,7 @@ const format = require('date-fns/format')
 
 
 router.get('/', (req, res) => {
-  let icons
-  Category.find().lean()
-    .then(categories => {
-      icons = new Map(categories.map(category => [category.category_name, category.icon])) // generate category => icon map
-
-    })
+  const categoryData = Category.find().lean()
 
   let filter
   if (!req.query.filter || req.query.filter === 'all') {
@@ -20,8 +15,11 @@ router.get('/', (req, res) => {
     filter = { category: req.query.filter }
   }
 
-  Record.find(filter).lean().sort({ date: 'desc' })
-    .then(records => {
+  const recordData = Record.find(filter).lean().sort({ date: 'desc' })
+
+  Promise.all([categoryData, recordData])
+    .then(([categories, records]) => {
+      const icons = new Map(categories.map(category => [category.category_name, category.icon])) // generate category => icon map
       const sumAmount = records.reduce((a, record) => a + record.amount, 0)
       const formatTotalAmount = new Intl.NumberFormat('en-US').format(sumAmount)
       const formatRecords = records.map(record => {
@@ -32,47 +30,16 @@ router.get('/', (req, res) => {
           amount: new Intl.NumberFormat('en-US').format(record.amount) // format amount
         }
       })
-
       res.render('index', {
         filter,
         records: formatRecords,
         totalAmount: formatTotalAmount,
       })
     })
-
+    .catch(err => console.log(err))
 })
 
-// router.get('/', (req, res) => {
-//   Category.find().lean()
-//     .then(categories => {
-//       let filter
-//       if (!req.query.filter || req.query.filter === 'all') {
-//         filter = null
-//       } else {
-//         filter = { category: req.query.filter }
-//       }
-//       Record.find(filter).lean().sort({ date: 'desc' })
-//         .then(records => {
-//           const icons = new Map(categories.map(category => [category.category_name, category.icon])) // generate category => icon map
-//           const sumAmount = records.reduce((a, record) => a + record.amount, 0)
-//           const formatTotalAmount = new Intl.NumberFormat('en-US').format(sumAmount)
-//           const formatRecords = records.map(record => {
-//             return {
-//               ...record,
-//               date: format(record.date, 'y/LL/dd'), // format date
-//               category: icons.get(record.category), // convert data to icon 
-//               amount: new Intl.NumberFormat('en-US').format(record.amount) // format amount
-//             }
-//           })
 
-//           res.render('index', {
-//             filter,
-//             records: formatRecords,
-//             totalAmount: formatTotalAmount,
-//           })
-//         })
-//     })
-// })
 
 // router.get('/', async (req, res) => {
 //   try {
