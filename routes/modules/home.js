fa02a3bb4359ee3b8296941f7e676b434306f192
Johnny Chang <zjps3407@gmail.com>
2020-12-30 17:26:15 +0800

    test Promise chaining

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,40 +5,109 @@ const Category = require('../../models/category')
 const format = require('date-fns/format')
 
 
-router.get('/', async (req, res) => {
-  try {
-    const categories = await Category.find().lean()
-    const icons = new Map(categories.map(category => [category.category_name, category.icon])) // generate category => icon map
+router.get('/', (req, res) => {
+  let icons
+  Category.find().lean()
+    .then(categories => {
+      icons = new Map(categories.map(category => [category.category_name, category.icon])) // generate category => icon map
 
-    let filter
-    if (!req.query.filter || req.query.filter === 'all') {
-      filter = null
-    } else {
-      filter = { category: req.query.filter }
-    }
-
-    const records = await Record.find(filter).lean().sort({ date: 'desc' })
-    const sumAmount = records.reduce((a, record) => a + record.amount, 0)
-    const formatTotalAmount = new Intl.NumberFormat('en-US').format(sumAmount)
-    const formatRecords = records.map(record => {
-      return {
-        ...record,
-        date: format(record.date, 'y/LL/dd'), // format date
-        category: icons.get(record.category), // convert data to icon 
-        amount: new Intl.NumberFormat('en-US').format(record.amount) // format amount
-      }
     })
 
-    res.render('index', {
-      filter,
-      records: formatRecords,
-      totalAmount: formatTotalAmount,
-    })
-
-  } catch (err) {
-    console.error(err)
+  let filter
+  if (!req.query.filter || req.query.filter === 'all') {
+    filter = null
+  } else {
+    filter = { category: req.query.filter }
   }
 
+  Record.find(filter).lean().sort({ date: 'desc' })
+    .then(records => {
+      const sumAmount = records.reduce((a, record) => a + record.amount, 0)
+      const formatTotalAmount = new Intl.NumberFormat('en-US').format(sumAmount)
+      const formatRecords = records.map(record => {
+        return {
+          ...record,
+          date: format(record.date, 'y/LL/dd'), // format date
+          category: icons.get(record.category), // convert data to icon 
+          amount: new Intl.NumberFormat('en-US').format(record.amount) // format amount
+        }
+      })
+
+      res.render('index', {
+        filter,
+        records: formatRecords,
+        totalAmount: formatTotalAmount,
+      })
+    })
+
 })
+
+// router.get('/', (req, res) => {
+//   Category.find().lean()
+//     .then(categories => {
+//       let filter
+//       if (!req.query.filter || req.query.filter === 'all') {
+//         filter = null
+//       } else {
+//         filter = { category: req.query.filter }
+//       }
+//       Record.find(filter).lean().sort({ date: 'desc' })
+//         .then(records => {
+//           const icons = new Map(categories.map(category => [category.category_name, category.icon])) // generate category => icon map
+//           const sumAmount = records.reduce((a, record) => a + record.amount, 0)
+//           const formatTotalAmount = new Intl.NumberFormat('en-US').format(sumAmount)
+//           const formatRecords = records.map(record => {
+//             return {
+//               ...record,
+//               date: format(record.date, 'y/LL/dd'), // format date
+//               category: icons.get(record.category), // convert data to icon 
+//               amount: new Intl.NumberFormat('en-US').format(record.amount) // format amount
+//             }
+//           })
+
+//           res.render('index', {
+//             filter,
+//             records: formatRecords,
+//             totalAmount: formatTotalAmount,
+//           })
+//         })
+//     })
+// })
+
+// router.get('/', async (req, res) => {
+//   try {
+//     const categories = await Category.find().lean()
+//     const icons = new Map(categories.map(category => [category.category_name, category.icon])) // generate category => icon map
+
+// let filter
+// if (!req.query.filter || req.query.filter === 'all') {
+//   filter = null
+// } else {
+//   filter = { category: req.query.filter }
+// }
+
+//     const records = await Record.find(filter).lean().sort({ date: 'desc' })
+// const sumAmount = records.reduce((a, record) => a + record.amount, 0)
+// const formatTotalAmount = new Intl.NumberFormat('en-US').format(sumAmount)
+// const formatRecords = records.map(record => {
+//   return {
+//     ...record,
+//     date: format(record.date, 'y/LL/dd'), // format date
+//     category: icons.get(record.category), // convert data to icon 
+//     amount: new Intl.NumberFormat('en-US').format(record.amount) // format amount
+//   }
+// })
+
+// res.render('index', {
+//   filter,
+//   records: formatRecords,
+//   totalAmount: formatTotalAmount,
+// })
+
+//   } catch (err) {
+//     console.error(err)
+//   }
+
+// })
 
 module.exports = router
