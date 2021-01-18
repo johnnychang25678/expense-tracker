f8acb386322b83d6a55f23121c49559a69fadd45
Johnny Chang <zjps3407@gmail.com>
2021-01-18 09:44:09 +0800

    add users module and login/register view

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.use('/', require('./modules/home'))
+router.use('/users', require('./modules/users'))
 router.use('/expenses', require('./modules/expenses'))
+router.use('/', require('./modules/home'))
 
 module.exports = router
