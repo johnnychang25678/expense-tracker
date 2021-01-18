5c6c9ec4b6c74ee827e4c0a680f7e5e674044431
Johnny Chang <zjps3407@gmail.com>
2021-01-18 19:00:14 +0800

    add google login

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
@@ -23,6 +24,29 @@ module.exports = app => {
         return done(err)
       }
     }))
+
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  }, async (accessToken, refreshToken, profile, done) => {
+    try {
+      const { email } = profile._json
+      const user = await User.findOne({ email })
+      if (user) return done(null, user)
+
+      const randomPassword = Math.random().toString(36).slice(-8)
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(randomPassword, salt)
+      const newUser = await User.create({
+        email,
+        password: hash
+      })
+      return done(null, newUser)
+    } catch (err) {
+      return done(err)
+    }
+  }))
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const authenticator = require('../middleware/auth')
 
 router.use('/users', require('./modules/users'))
+router.use('/auth', require('./modules/auth'))
 router.use('/expenses', authenticator, require('./modules/expenses'))
 router.use('/', authenticator, require('./modules/home'))
 
